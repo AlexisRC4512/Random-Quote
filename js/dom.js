d6c581d7ac1aebec3d2f0b37ef031599509e9a40
AlexisRC <etherio4512@hotmail.com>
2021-11-18 21:43:14 -0500

    end

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,7 @@
 import printApi from "./fetch.js";
-
 const d=document;
-d.addEventListener('click',(e)=>{
+const btnRandom=d.getElementById("btn-random");
+btnRandom.addEventListener('click',(e)=>{
     printApi("#btn-random");
     e.preventDefault();
 })
